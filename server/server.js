5233d5c76ad7c95ed213ef038b94502de70f2e59
Christian Allred <callred@myfluenthome.com>
2019-05-02 20:26:13 -0600

    asdf

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const setup = async () => {
     .use(serveStatic(path.join(__dirname, "../build")));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 };
 
